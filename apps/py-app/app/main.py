ed746f400eb91172a5c0f34e33713b5f0ffcfb05
Yuval Shavit <yuval.shavit@klo.dev>
2023-03-07 19:16:11 -0500

    switch import to supported version

--- apps/py-app/app/main.py
+++ apps/py-app/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app import secret
+from app.secret import get_secret
 
 from starlette.responses import PlainTextResponse
 
@@ -12,9 +12,9 @@ app = FastAPI()
 
 @app.get("/test/persist-secret/read-binary-secret")
 async def get_secret_binary():
-    return await secret.get_secret(binary=True)
+    return await get_secret(binary=True)
 
 
 @app.get("/test/persist-secret/read-text-secret", response_class=PlainTextResponse)
 async def get_secret_text():
-    return await secret.get_secret()
+    return await get_secret()
